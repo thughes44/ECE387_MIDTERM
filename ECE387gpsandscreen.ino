4bb6eb4130901f7b9fff27875ce4020fa961b814
Tyler <hughest6@miamioh.edu>
2019-02-28 15:28:21 -0500

    Add files via upload

---
+++ ECE387gpsandscreen.ino
@@ -0,0 +1,119 @@
+/*
+  ECE 387 MIDTERM PROJECT
+  TYLER HUGHES
+
+  Program to interact with GPS and output informtion to a 16x2 LCD screen
+  This project makes extensive use of TinyGPS library and its included examples
+*/
+
+// Necessary libraries
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+
+// Setup pins for display
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// Pins for GPS interaction
+static const int RXPin = 9, TXPin = 8;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup()
+{
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2); 
+  // Default screen to lock aquire, changes when locked
+  lcd.print("AQUIRING SAT");
+  lcd.setCursor(0,1);                                                                                                                                                                          QCCC
+  lcd.print("LOCK");
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+
+  // print library information to serial monitor
+  Serial.println(F("DeviceExample.ino"));
+  Serial.println(F("A simple demonstration of TinyGPS++ with an attached GPS module"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+}
+
+void loop()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0) {
+    if (gps.encode(ss.read())){
+      displayInfo();
+    }
+  }
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+// Main function to display information to both serial monitor and LCD
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    lcd.setCursor(0,0);
+    lcd.print("LAT: ");
+    lcd.print(gps.location.lat(), 6);
+    lcd.setCursor(0,1);
+    lcd.print("LNG: ");
+    lcd.print(gps.location.lng(), 6);
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
